8891feeaa30b605e8b34b3de2292b8099418c148
ae <1141239384@qq.com>
2018-07-25 00:29:07 +0800

    helloworld first try

---
+++ helloworld/src/zcl/xjtu.java
@@ -0,0 +1,11 @@
+package zcl;
+
+/**
+ * Created by ae on 2018/7/24.
+ */
+public class xjtu {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("zcl");
+    }
+}
